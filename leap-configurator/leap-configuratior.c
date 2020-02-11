60636b14bdd7290f01ea5b49bdac259fc6b26d6a
Maho Morichika <nat@magicnat.com>
2020-02-11 16:28:58 -0500

    leap-conf: debug hands

--- leap-configurator/leap-configuratior.c
+++ leap-configurator/leap-configuratior.c
@@ -1,8 +1,19 @@
 #include "leapio/leapio.h"
 #include "log.h"
 
+void log_tracks(const LEAP_TRACKING_EVENT *ev) {
+    log_debug("saw %u hands.\n", ev->nHands);
+    for(uint32_t h = 0; h < ev->nHands; h++) {
+        const LEAP_HAND* hand = &(ev->pHands[h]);
+        log_debug("hand %u is a %s hand. location (%f, %f, %f).\n", 
+            hand->id, hand->type == eLeapHandType_Left ? "left" : "right",
+            hand->palm.position.x, hand->palm.position.y, hand->palm.position.z);
+    }
+}
+
 int main () {
     log_info("connecting to leap service...\n");
+    leap_set_tracking_handler(log_tracks); // debug
 
     leap_connect(NULL);
     while (!leap_is_connected()) {
@@ -15,6 +26,8 @@ int main () {
         Sleep(10);
     }
     log_info("leap device connected.\n");
+
+    join(); 
 
     // TODO
 
