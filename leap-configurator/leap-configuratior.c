863f9f21eb069884ebddbaae0500a7d49ef463e2
Maho Morichika <nat@magicnat.com>
2020-02-11 00:06:34 -0500

    leap-conf: wait for device.

--- leap-configurator/leap-configuratior.c
+++ leap-configurator/leap-configuratior.c
@@ -2,15 +2,25 @@
 #include "log.h"
 
 int main () {
-    log_info("waiting for leap device...\n");
+    log_info("connecting to leap service...\n");
 
     leap_connect(NULL);
     while (!leap_is_connected()) {
         Sleep(10);
     }
+    log_info("connected to leap service.\n");
+
+    log_info("waiting for any leap device...\n");
+    while (!leap_is_device_connected()) {
+        Sleep(10);
+    }
+    log_info("leap device connected.\n");
 
     // TODO
 
+    log_info("disconnecting from leap service...\n");
+    leap_disconnect();
+    log_info("disconnected.\n");
     return 0;
 }
 
